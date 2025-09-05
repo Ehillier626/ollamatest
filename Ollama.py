fd7a842235d605ff7e54b2838eef45346a089b48
Mark O <Moduca@rrc.ca>
2025-09-05 11:37:29 -0500

    added prompt.txt

--- Ollama.py
+++ Ollama.py
@@ -8,24 +8,16 @@ upload = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 # function feeds image data into ollama
 
 
+Prompt = open("Prompt.txt")
+
+
 def llm_read(image_data):
 
     response = ollama.chat(
         model="gemma3:4b",
         messages=[{
             "role": "user",
-            "content": "Please Extract all relevant resume information from "
-            "the uploaded image."
-            " If it doesn't look like a resume please inform user."
-            " Relevant info would be"
-            "things like experience, phone and email, please put all info you "
-            "find into a table with catagories for each."
-            " Make sure to get as much relevant info as possible."
-            " Do not add any duplicates or additional notes"
-            "Only list relevant info NO extra information or notes at all"
-            "Keep it concise"
-            "if There is no data for specific criteria then don't add it to the table"
-            "DO NOT ADD A LARGE AMOUNT OF TABLES (MORE THAN 10) AND MAKE SURE EACH TABLE DOES NOT JUST SAY NONE",
+            "content": Prompt,
             "images": [image_data]
         }]
     )
